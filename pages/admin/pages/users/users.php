821aeecb78290ea148d131aac2810361599a1463
maximdudai <maxim.dudai01@gmail.com>
2023-01-22 20:43:08 +0000

    Manage Users
    created form for changing user information
    
    form will automatically be filled with DB info
    
    created table with user orders
    
    added new button to remove user from database.
    
    if selected user to edit is logged in user, delete account button will be not available.
    
    added new button to change password visibility

--- pages/admin/pages/users/users.php
+++ pages/admin/pages/users/users.php
@@ -14,9 +14,42 @@
     
     <?php require($_SERVER['DOCUMENT_ROOT'].'/shop/components/navbar/navbar.php'); ?>
 
-    <div class="continer">
+    <div class="container mt-5">
         <div class="row">
-            
+            <table class="table text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Second Name</th>
+                        <th scope="col">Manage</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $getUsersFromDb = "SELECT * FROM `users` ORDER BY ID";
+                        $getUsers = mysqli_query($sql, $getUsersFromDb);
+
+                        if(mysqli_num_rows($getUsers)) {
+
+                            while($row = mysqli_fetch_array($getUsers)) {
+                                echo 
+                                '
+                                    <tr>
+                                        <th scope="row">'.$row['ID'].'</th>
+                                        <td>'.$row['fName'].'</td>
+                                        <td>'.$row['lName'].'</td>
+                                        <td>
+                                            <a href="./edit_user/edit_user.php?user='.$row['ID'].'"><span class="material-symbols-outlined" role="button">settings</span></a>
+                                        </td>
+                                    </tr>
+                                ';
+                            }
+
+                        }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
 
