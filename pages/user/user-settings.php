4fb91c18941d75521312ee7b0bfdfeaaa7066e5f
maximdudai <maxim.dudai01@gmail.com>
2023-01-24 22:44:34 +0000

    User Settings
    Created Settings page with default components

---
+++ pages/user/user-settings.php
@@ -0,0 +1,34 @@
+<?php 
+    error_reporting(0);
+    session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php 
+        require($_SERVER['DOCUMENT_ROOT'].'/shop/components/head.php');
+    ?>
+<body>
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/shop/components/navbar/navbar.php'); ?>
+
+    <div class="container m-5">
+
+        <div class="row d-flex flex-column justify-content-between align-items-center">
+            
+            <div class="col-md-5">
+
+            </div>
+
+            <div class="col-md-5">
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/shop/components/footer/footer.php'); ?>
+</body>
+</html>
