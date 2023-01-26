141666b6891717e27f7b6e5ee3f9546baa7f7cd5
maximdudai <maxim.dudai01@gmail.com>
2023-01-26 23:45:58 +0000

    Books Management
    Create page UI default components

---
+++ pages/admin/pages/books/books.php
@@ -0,0 +1,20 @@
+<?php 
+    error_reporting(0);
+    session_start();
+
+    require($_SERVER['DOCUMENT_ROOT'].'/shop/connection/database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php 
+        require($_SERVER['DOCUMENT_ROOT'].'/shop/components/head.php');
+    ?>
+<body>
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/shop/components/navbar/navbar.php'); ?>
+
+    
+
+    <?php require($_SERVER['DOCUMENT_ROOT'].'/shop/components/footer/footer.php'); ?>
+</body>
+</html>
