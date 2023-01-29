f24a319a7971e6d27d05222c67a1e760042ce091
maximdudai <maxim.dudai01@gmail.com>
2023-01-29 09:08:54 +0000

    About us page

--- pages/about/about.php
+++ pages/about/about.php
@@ -14,8 +14,33 @@
     <?php require($_SERVER['DOCUMENT_ROOT'].'/shop/components/navbar/navbar.php'); ?>
 
     <div class="container mt-5">
-        <div class="row justify-content-center">
+        <div class="row d-flex flex-column justify-content-center align-items-center">
             
+        <div class="col-md-7">
+                <div class="title">
+                    WHO WE ARE
+                    <hr />
+                </div>
+                <div class="content">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsam dolorem laboriosam, dicta nesciunt quod cum eum saepe odio suscipit accusamus iste architecto tempora non excepturi eaque. Provident laudantium voluptatibus odio rem, deserunt saepe dolore perspiciatis laboriosam nemo ipsa eaque necessitatibus itaque ea iusto quia, labore alias! Nemo, repudiandae ipsa?
+                </div>
+            </div>
+
+            <div class="col-md-7 mt-5">
+                <div class="title">
+                    ABOUT OUR EBOOKS SHOP
+                    <hr />
+                </div>
+                <div class="content">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus doloribus iure omnis ullam necessitatibus dolorum architecto mollitia libero nesciunt iste culpa ad quibusdam suscipit fugit similique quae repellendus, eius accusantium ipsum earum voluptatum quas ipsa. Reprehenderit vel, porro voluptatum at nostrum quo nisi? Provident adipisci vel explicabo maxime quia ullam?
+                </div>
+            </div>
+
+            <div class="col-md-5 mt-5 text-center">
+                <a href="../library/library.php" class="btn btn-lg btn-outline-success">SHOP</a>
+                <a href="../contact/contact.php" class="btn btn-lg btn-outline-success">Contact US</a>
+            </div>
+
         </div>
     </div>
 
